387ff11904fb3e608d553092ad0f21876610ab07
MarvinJWendt <github@marvinjwendt.com>
2018-12-17 00:11:34 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const log = console.log
+
+exports.watch = args => {
+    fs.watch('./', {recursive: true}, (event, filename) => {
+        if(!filename) log.error('No filename (?)')
+    
+        if(event == 'rename') log.rename(filename)
+        else if(event == 'change') log.change(filename)
+    
+    })  
+}
+
+log.error = message => {
+    log(chalk.red('Error: ') + message)
+}
+
+log.change = filename => {
+    log(`${chalk.green('Change:')} ${filename}`)
+}
+
+log.rename = filename => {
+    log(`${chalk.magenta('Rename:')} ${filename}`)
+}
