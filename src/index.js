fa9400d3f140e8b532c132559113f195e6b8a778
MarvinJWendt <github@marvinjwendt.com>
2018-12-17 00:14:22 +0100

    Prettier :lipstick:

--- src/index.js
+++ src/index.js
@@ -4,23 +4,22 @@ const chalk = require('chalk')
 const log = console.log
 
 exports.watch = args => {
-    fs.watch('./', {recursive: true}, (event, filename) => {
-        if(!filename) log.error('No filename (?)')
-    
-        if(event == 'rename') log.rename(filename)
-        else if(event == 'change') log.change(filename)
-    
-    })  
+  fs.watch('./', { recursive: true }, (event, filename) => {
+    if (!filename) log.error('No filename (?)')
+
+    if (event == 'rename') log.rename(filename)
+    else if (event == 'change') log.change(filename)
+  })
 }
 
 log.error = message => {
-    log(chalk.red('Error: ') + message)
+  log(chalk.red('Error: ') + message)
 }
 
 log.change = filename => {
-    log(`${chalk.green('Change:')} ${filename}`)
+  log(`${chalk.green('Change:')} ${filename}`)
 }
 
 log.rename = filename => {
-    log(`${chalk.magenta('Rename:')} ${filename}`)
+  log(`${chalk.magenta('Rename:')} ${filename}`)
 }
